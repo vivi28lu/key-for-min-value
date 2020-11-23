86a03c288a893af015c1becb086a61438ba56350
viviana <vivi28lu@gmail.com>
2020-11-22 20:24:23 -0500

    commit tree 7c4219cc1ca27368118ef4727e0eecdc859649d9

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,8 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+name_hash = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
-
+    name_hash.collect do |furniture, amount|
+        furniture
+    end
 end
